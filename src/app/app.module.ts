9705a2948b5a945aea16009a677058ccbc3cea90
Git290234 <viktorkuenstler@googlemail.com>
2022-12-25 22:46:10 +0100

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './router-module/AppRoutingModule';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar/toolbar.component';
 import { HelloComponent } from './hello.component';
 import { ListOfProductsComponent } from './backend/list-of-products/list-of-products.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, AppRoutingModule, MatToolbarModule],
-  declarations: [AppComponent, HelloComponent, ListOfProductsComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+  ],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    ListOfProductsComponent,
+    ToolbarComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
