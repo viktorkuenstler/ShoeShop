7e0d58b5dd13f9856f20e7df74ef6260dd36d9a9
Git290234 <viktorkuenstler@googlemail.com>
2022-12-26 12:51:32 +0100

    5 files modified

--- src/app/DataTransferObjects/Product.ts
+++ src/app/DataTransferObjects/Product.ts
@@ -1,12 +1,8 @@
-class Product {
-
+export class Product {
   constructor(
-            public articleNumer: string,
-            public name: string,
-            public yearOfPublication: Date,
-            public price: number
-            ){
-
-            }
-
+    public articleNumer: string,
+    public name: string,
+    public yearOfPublication: Date,
+    public price: number
+  ) {}
 }
--- src/app/backend/list-of-products/list-of-products.component.ts
+++ src/app/backend/list-of-products/list-of-products.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../../DataTransferObjects/Product';
 
 @Component({
   selector: 'app-list-of-products',
   templateUrl: './list-of-products.component.html',
-  styleUrls: ['./list-of-products.component.css']
+  styleUrls: ['./list-of-products.component.css'],
 })
 export class ListOfProductsComponent implements OnInit {
 
-  constructor() { }
+  products: Array<Product> = [
+    new Product('A100', 'SuperSneaker1', new Date(2022, 1), 100),
+    new Product('A200', 'SuperSneaker2', new Date(2022, 2), 200),
+    new Product('A300', 'SuperSneaker3', new Date(2022, 3), 300),
+  ];
 
-  ngOnInit() {
-  }
+  constructor() {}
 
+  ngOnInit() {}
 }
