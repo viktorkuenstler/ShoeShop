c648294b373041340b78aa8dcd886f256be4036c
Git290234 <viktorkuenstler@googlemail.com>
2022-12-26 08:21:53 +0100

    4 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './router-module/AppRoutingModule';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './toolbar/toolbar/toolbar.component';
 import { HelloComponent } from './hello.component';
 import { ListOfProductsComponent } from './backend/list-of-products/list-of-products.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 @NgModule({
   imports: [
@@ -17,12 +22,17 @@ import { ListOfProductsComponent } from './backend/list-of-products/list-of-prod
     AppRoutingModule,
     MatToolbarModule,
     MatIconModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatSidenavModule,
+    BrowserAnimationsModule
   ],
   declarations: [
     AppComponent,
     HelloComponent,
     ListOfProductsComponent,
     ToolbarComponent,
+    SidenavComponent,
   ],
   bootstrap: [AppComponent],
 })
